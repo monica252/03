826b7e732a32568aad61a64f9570f9472ef72586
monica252 <dadada74@gmail.com>
2021-04-28 11:25:56 +0900

    clone

---
+++ hint.php
@@ -0,0 +1,12 @@
+<?php
+echo max_array([2, 3, 5, 4]); //5 が表示されればok
+//hogehogeの部分を考える
+function max_array($nums){
+  $max_num = $nums[0];
+  foreach($nums as $num){
+	if(hogehoge){
+	  hogehoge
+	}
+  }
+  return $max_num;
+}
